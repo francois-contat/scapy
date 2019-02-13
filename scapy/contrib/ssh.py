77f262a5329a1c99471f1b321ad0ac39128e2efa
Francois Contat <francois.contat@gmail.com>
2019-02-13 17:25:41 +0100

    ajout du paquet générique et du premier message d'echanges d'algo

--- scapy/contrib/ssh.py
+++ scapy/contrib/ssh.py
@@ -35,12 +35,32 @@
 import struct
 
 from scapy.packet import Packet, bind_layers, Raw
-from scapy.fields import StrField
+from scapy.fields import StrField, StrLenField, IntField, FieldLenField
+from scapy.fields import ByteField, StrFixedLenField
+from scapy.compat import orb
+
 #from scapy.fields import ByteEnumField, ByteField, IntField, ShortField
 #from scapy.fields import IPField, IP6Field, StrLenField
 #from scapy.fields import FieldLenField
 #from scapy.fields import StrFixedLenField, ShortEnumField
 #from scapy.layers.inet import TCP
+
+COMPRESSION = {0: "none",
+               1: "zlib"}
+
+# TODO : faire les longueurs
+HMAC_LENGTH = {'sha1': 20}
+
+CIPHER = {0:0}
+
+SSH_MESSAGE = {1:'disconnect',
+               2:'ignore',
+               3:'unimplemented',
+               4:'debug',
+               5:'service_request',
+               6:'service_accept',
+               20:'kexinit',
+               21:'newkeyx'}
 
 #STATIC_END_OF_DATA_V1_LENGTH = 24
 #
@@ -86,23 +106,70 @@ from scapy.fields import StrField
 #            pkt = pkt[:2] + struct.pack('!I', temp_len) + pkt[6:]
 #        return pkt + pay
 
+class SSHBinaryPacket(Packet):
+    '''
+        Generic Binary SSH packet
+        https://tools.ietf.org/html/rfc4253#section-6
+    '''
+    name = 'SSH Generic Binary'
+    fields_desc = [IntField('packet_length', 16),
+                   FieldLenField('padding_length', 0, fmt='B', length_of ='padding'),
+                   StrLenField('payload', '', length_from = lambda x: x.packet_length - x.padding_length - 1),
+                   StrLenField('padding', '', length_from = lambda x: x.padding_length),
+                   StrLenField('mac', '')] # TODO : length a calculer selon le cipher choisi (super...)
+
+    # note : length of the concatenation of 'packet_length', 'padding_length', 'payload', and 'random padding' MUST be a multiple of the cipher block size or 8, whichever is larger
+
+
+class SSHKeyExchange(Packet):
+    '''
+        SSH Key Exchange packet
+        https://tools.ietf.org/html/rfc4253#section-7.1
+    '''
+    name = 'SSH Key Exchange'
+    fields_desc = [IntField('packet_length', 16),
+                   FieldLenField('padding_length', 0, fmt='B', length_of ='padding'),
+                   ByteEnumField('message', 20, SSH_MESSAGE),
+                   StrFixedLenField('cookie', b'\x00'*16, 16),
+                   FieldLenField('kex_algorithms_length', 0, fmt='I', length_of='kex_algorithms'),
+                   StrLenField('kex_algorithms', '', length_from = lambda x:x.kex_algorithms_length),
+                   FieldLenField('server_host_key_algorithms_length', 0, fmt='I', length_of='server_host_key_algorithms'),
+                   StrLenField('server_host_key_algorithms', '', length_from = lambda x:x.server_host_key_algorithms_length),
+                   FieldLenField('encryption_algorithms_client_to_server_length', 0, fmt='I', length_of='encryption_algorithms_client_to_server'),
+                   StrLenField('encryption_algorithms_client_to_server', '', length_from = lambda x:x.encryption_algorithms_client_to_server_length),
+                   FieldLenField('encryption_algorithms_server_to_client_length', 0, fmt='I', length_of='encryption_algorithms_server_to_client'),
+                   StrLenField('encryption_algorithms_server_to_client', '', length_from = lambda x:x.encryption_algorithms_server_to_client_length),
+                   FieldLenField('mac_algorithms_client_to_server_length', 0, fmt='I', length_of='mac_algorithms_client_to_server'),
+                   StrLenField('mac_algorithms_client_to_server', '', length_from = lambda x:x.mac_algorithms_client_to_server_length),
+                   FieldLenField('mac_algorithms_server_to_client_length', 0, fmt='I', length_of='mac_algorithms_server_to_client'),
+                   StrLenField('mac_algorithms_server_to_client', '', length_from = lambda x:x.mac_algorithms_server_to_client_length),
+                   FieldLenField('compression_algorithms_client_to_server_length', 0, fmt='I', length_of='compression_algorithms_client_to_server'),
+                   StrLenField('compression_algorithms_client_to_server', '', length_from = lambda x:x.compression_algorithms_client_to_server_length),
+                   FieldLenField('compression_algorithms_server_to_client_length', 0, fmt='I', length_of='compression_algorithms_server_to_client'),
+                   StrLenField('compression_algorithms_server_to_client', '', length_from = lambda x:x.compression_algorithms_server_to_client_length),
+                   FieldLenField('languages_client_to_server_length', 0, fmt='I', length_of='languages_client_to_server'),
+                   StrLenField('languages_client_to_server', '', length_from = lambda x:x.languages_client_to_server_length),
+                   FieldLenField('languages_server_to_client_length', 0, fmt='I', length_of='languages_server_to_client'),
+                   StrLenField('languages_server_to_client', '', length_from = lambda x:x.languages_server_to_client_length),
+                   ByteField('first_kex_packet_follows', 0),
+                   IntField('reserved', 0),
+                   StrLenField('padding', '', length_from = lambda x: x.padding_length)]
+
+
+
 class SSHVersionExchange(Packet):
     '''
-      SSH Version Exchange packet
-      eg https://tools.ietf.org/html/rfc4253#section-4.2
+        SSH Version Exchange packet
+        https://tools.ietf.org/html/rfc4253#section-4.2
     '''
     name = 'SSH Version Exchange'
-    #Naive version
-    #fields_desc = [StrStopField('version', None, b"\x0d\x0a", 0)]
-    #Enhanced version
-    fields_desc = [FieldListField('comments', None, StrStopField('version', None, b"\x0d\x0a", 0))] 
-
+    fields_desc = [FieldListField('comments', ['SSH-2.0-scapySSH_1.0\x0d\x0a'], StrStopField('version', None, b"\x0d\x0a", 0))] 
 
 
 class SSH(Packet):
     '''
-    Dummy SSH packet for SSH dissection
-    according to https://tools.ietf.org/html/rfc4253
+        Dummy SSH packet for SSH dissection
+        https://tools.ietf.org/html/rfc4253
     '''
     name = 'SSH'
 
@@ -114,7 +181,11 @@ class SSH(Packet):
         if len(_pkt) >= 4:
             if ''.join(map(str, struct.unpack('cccc', _pkt[:4]))) == 'SSH-':
                 return SSHVersionExchange
-        return Raw
+            elif len(_pkt) >= 5:
+                if orb(_pkt[5]) == 20:
+                    return SSHKeyExchange
+        return SSHBinaryPacket
+
 
 bind_layers(TCP, SSH, dport=22)  # reserved port
 bind_layers(TCP, SSH, sport=22)  # reserved port
